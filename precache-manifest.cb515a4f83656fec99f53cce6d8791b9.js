a4b6ebf9a6479c0de7aa7bf6b56fa2f461a30981
Haris Raza <harisraza292@gmail.com>
2020-06-26 20:36:03 +0500

    Updates

---
+++ precache-manifest.cb515a4f83656fec99f53cce6d8791b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a604ab6467f29baad08abe17e7381ab",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "f7fb2f60a59cfbc42409",
+    "url": "/react-movie-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "42423b98a7df43bda8c7",
+    "url": "/react-movie-app/static/css/main.0c3736cc.chunk.css"
+  },
+  {
+    "revision": "f7fb2f60a59cfbc42409",
+    "url": "/react-movie-app/static/js/2.ff99a681.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movie-app/static/js/2.ff99a681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42423b98a7df43bda8c7",
+    "url": "/react-movie-app/static/js/main.96cb5364.chunk.js"
+  },
+  {
+    "revision": "68581cf6d528f11502ca",
+    "url": "/react-movie-app/static/js/runtime-main.53d64990.js"
+  }
+]);
